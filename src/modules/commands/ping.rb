0a29f0f83b4fcb528518d979e0c79e1c41436076
Birdie0 <Birdie0@users.noreply.github.com>
2017-02-15 15:57:50 +0200

    Update ping.rb
    Changed to `pong with editing` because previous one respond with self-ping delay (80-150 ms) and shows wrong numbers.
    Also it uses seconds by default so it multiplied by 1000 for milliseconds.

--- src/modules/commands/ping.rb
+++ src/modules/commands/ping.rb
@@ -4,7 +4,10 @@ module Bot
     # This used to check if bot is alive
     module Ping
       extend Discordrb::Commands::CommandContainer
-      command(:ping ) { |event| "`#{Time.now - event.timestamp}ms`" }
+      command :ping do |event|
+        m = event.send 'Pong!'
+        m.edit "Pong! `#{(Time.now - event.timestamp) * 1000} ms`"
+      end
     end
   end
 end
