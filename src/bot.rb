deb8ea487d600de425ae6a4279e734ae766b3472
Zac Nowicki <zachnowicki@gmail.com>
2017-08-17 16:57:12 +0000

    document Bot.load_modules

--- src/bot.rb
+++ src/bot.rb
@@ -18,6 +18,14 @@ module Bot
                                             token: CONFIG.token,
                                             prefix: CONFIG.prefix)
 
+  # This class method wraps the module lazy-loading process of discordrb command
+  # and event modules. Any module name passed to this method will have its child
+  # constants iterated over and passed to `Discordrb::Commands::CommandBot#include!`
+  # Any module name passed to this method *must*:
+  #   - extend Discordrb::EventContainer
+  #   - extend Discordrb::Commands::CommandContainer
+  # @param klass [Symbol, #to_sym] the name of the module
+  # @param path [String] the path underneath `src/modules/` to load files from
   def self.load_modules(klass, path)
     new_module = Module.new
     const_set(klass.to_sym, new_module)
